b2c594e30d06f3e5282c3ae3e463718b536005b7
hermenoiesis-Vaio <hermenoiesis@gmail.com>
2026-08-30 04:05:31 +0300

    Sync quartz config and update graph routes

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "hermenoiesis.github.io",
+    baseUrl: "hermenoiesis.github.io/PKM",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
